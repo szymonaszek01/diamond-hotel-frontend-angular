4714cb8fe9481d6b6bff441f82cd9a3648c56b54
szymonaszek01 <235879@edu.p.lodz.pl>
2023-05-10 23:29:34 +0200

    task-008 Adding new model - "User profile model"

---
+++ src/app/model/UserProfile.ts
@@ -0,0 +1,25 @@
+export interface UserProfile {
+  id: number;
+
+  email: string;
+
+  firstname: string;
+
+  lastname: string;
+
+  age: number;
+
+  country: string;
+
+  passportNumber: string;
+
+  phoneNumber: string;
+
+  city: string;
+
+  street: string;
+
+  postalCode: string;
+
+  role: string;
+}
