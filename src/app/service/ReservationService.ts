8f10188ee8ed7699caad0a7fa4bbc9d2cb237708
szymonaszek01 <235879@edu.p.lodz.pl>
2023-06-01 10:53:01 +0200

    task-047 Adding new services required to "New reservation" process

--- src/app/service/ReservationService.ts
+++ src/app/service/ReservationService.ts
@@ -4,6 +4,9 @@ import {Observable} from "rxjs";
 import {UserReservationAllResponseDto} from "../dto/reservation/UserReservationAllResponseDto";
 import {UserReservationAllRequestDto} from "../dto/reservation/UserReservationAllRequestDto";
 import {UserReservationInfo} from "../model/reservation/UserReservationInfo";
+import {UserReservationNewRequestDto} from "../dto/reservation/UserReservationNewRequestDto";
+import {UserReservationNewResponseDto} from "../dto/reservation/UserReservationNewResponseDto";
+import {ShoppingCartModel} from "../model/shopping-cart/ShoppingCartModel";
 
 @Injectable({
   providedIn: 'root'
@@ -23,17 +26,42 @@ export class ReservationService {
     return this.http.post<UserReservationAllResponseDto>(this.url + '/all/info', userReservationAllRequestDto);
   }
 
+  public createNewReservation(userReservationNewRequestDto: UserReservationNewRequestDto): Observable<UserReservationNewResponseDto> {
+    return this.http.post<UserReservationNewResponseDto>(this.url + '/create/new', userReservationNewRequestDto);
+  }
+
   public toUserReservationInfoListMapper(userReservationAllResponseDto: UserReservationAllResponseDto): UserReservationInfo[] {
     return userReservationAllResponseDto.reservation_list.map(userReservationAllResponseDto => {
       return {
         id: userReservationAllResponseDto.id,
-        code: userReservationAllResponseDto.code,
+        transactionCode: userReservationAllResponseDto.transaction_code,
         roomType: userReservationAllResponseDto.room_type_name,
         checkIn: userReservationAllResponseDto.check_in,
         checkOut: userReservationAllResponseDto.check_out,
         capacity: userReservationAllResponseDto.capacity,
-        cost: userReservationAllResponseDto.cost
+        roomCost: userReservationAllResponseDto.room_cost
       }
     });
+  }
+
+  public toUserReservationNewRequestDtoMapper(shoppingCartModel: ShoppingCartModel): UserReservationNewRequestDto {
+    return {
+      user_profile_id: shoppingCartModel.userProfileId,
+      check_in: shoppingCartModel.checkIn,
+      check_out: shoppingCartModel.checkOut,
+      room_type_info: shoppingCartModel.roomTypeSummaryInfo.map(roomTypeSummary => {
+        return {
+          room_type_name: roomTypeSummary.roomTypeCardData.roomTypeName,
+          selected_rooms: roomTypeSummary.roomTypeCardData.amount
+        }
+      }),
+      flight_number: shoppingCartModel.flightNumber,
+      cost: {
+        total_without_tax: shoppingCartModel.totalWithoutTax,
+        car_rent: shoppingCartModel.carRentCost,
+        car_pick_up: shoppingCartModel.carPickUpCost,
+        tax: shoppingCartModel.tax
+      }
+    };
   }
 }
--- src/app/service/TransactionService.ts
+++ src/app/service/TransactionService.ts
@@ -1,26 +1,37 @@
 import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {TransactionStatusInfoDto} from "../dto/transaction/TransactionStatusInfoDto";
 
 @Injectable({
   providedIn: 'root'
 })
+export class TransactionService {
 
-export class PaymentService {
+  // URL_PRODUCTION
+  private url = 'https://diamond-hotel-backend.onrender.com/api/v1/transaction';
 
-  private url: string = 'https://checkout.stripe.com/checkout.js';
+  // URL_LOCALHOST
+  // private url = 'http://localhost:5432/api/v1/transaction';
 
-  private api_key: string = 'pk_test_51NCOIJHClfuzSQRfRrX2oW3tN4OQBgzAHgbbxFrOZvEmc2uoZ0Jlk87UNmBV1SteuRKFJQfJzEpcoI0c39c4LWTM00lvEcKXkR';
+  private payment: string = 'https://checkout.stripe.com/checkout.js';
+
+  private key: string = 'pk_test_51NCOIJHClfuzSQRfRrX2oW3tN4OQBgzAHgbbxFrOZvEmc2uoZ0Jlk87UNmBV1SteuRKFJQfJzEpcoI0c39c4LWTM00lvEcKXkR';
 
   paymentHandler: any = null;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.invokeStripe();
+  }
+
+  public changeTransactionStatus(transactionStatusInfoDto: TransactionStatusInfoDto): Observable<TransactionStatusInfoDto> {
+    return this.http.post<TransactionStatusInfoDto>(this.url + '/change/status', transactionStatusInfoDto);
   }
 
   public initializePayment(title: string, description: string, amount: number): Observable<any> {
     return new Observable((observer) => {
       const paymentHandler = (<any>window).StripeCheckout.configure({
-        key: this.api_key,
+        key: this.key,
         locale: 'auto',
         token: function (stripeToken: any) {
           observer.next(stripeToken);
@@ -41,7 +52,7 @@ export class PaymentService {
       const script = window.document.createElement("script");
       script.id = "stripe-script";
       script.type = "text/javascript";
-      script.src = this.url;
+      script.src = this.payment;
       script.onload = () => {
         this.paymentHandler = (<any>window).StripeCheckout.configure({
           key: 'pk_test_51NCOIJHClfuzSQRfRrX2oW3tN4OQBgzAHgbbxFrOZvEmc2uoZ0Jlk87UNmBV1SteuRKFJQfJzEpcoI0c39c4LWTM00lvEcKXkR',
